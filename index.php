bf0e3e9f319ee30937611968fab22efb313b0e8a
Komiya Yu <komiya.yu@n138.jp>
2024-05-14 12:07:54 +0900

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,9 @@ date_default_timezone_set('UTC');
 session_name('token'.date('Ymd-H'));
 session_start();
 
+$system = [
+	'hash_aglos' => 'sha1',
+];
 $result = [];
 $result += [ 'issueat' => time() ];
 $result += [ 'client' => [
@@ -16,10 +19,46 @@ $result += [ 'client' => [
 $result += [ 'request' => [
 	'uri' => $_SERVER['REQUEST_URI'],
 	'query' => $_SERVER['QUERY_STRING'],
-	'method' => $_SERVER['REQUEST_METHOD'],
-	'uuid' => hash('sha1', $result['issueat']) . '_' . $_SERVER['UNIQUE_ID'],
+	'method' => mb_strtolower($_SERVER['REQUEST_METHOD']),
+	'uuid' => hash($system['hash_aglos'], $result['issueat']) . '_' . $_SERVER['UNIQUE_ID'],
 ] ];
 
 header('Content-Type: application/json');
+if ( ! in_array( $result['request']['method'], ['get'] ) ) { 
+	$result += [ 'erroron' => __LINE__ ];
+	echo json_encode($result);
+	exit();
+}
+if ( ! isset($_REQUEST) ) {
+	$result += [ 'erroron' => __LINE__ ];
+	echo json_encode($result);
+	exit();
+}
+if ( ! isset($_REQUEST['q']) ) {
+	$result += [ 'erroron' => __LINE__ ];
+	echo json_encode($result);
+	exit();
+}
+$result['request'] = array_merge($result['request'], [ 'text' => $_REQUEST['q'] ]);
+$result += [ 'result' => [ 'text' => $_REQUEST['q'] ] ];
+
+if ( strpos($_REQUEST['q'], 'https://x.com/') !== FALSE ) {
+	$result['result'] = array_merge(
+		$result['result'], ['text' => str_replace( 'https://x.com/', 'https://fxtwitter.com/', $_REQUEST['q'] )],
+	);
+}
+if ( strpos($_REQUEST['q'], 'https://twitter.com/') !== FALSE ) {
+	$result['result'] = array_merge(
+		$result['result'], ['text' => str_replace( 'https://twitter.com/', 'https://fxtwitter.com/', $_REQUEST['q'] )],
+	);
+}
+if ( strpos($_REQUEST['q'], 'https://vwitter.com/') !== FALSE ) {
+	$result['result'] = array_merge(
+		$result['result'], ['text' => str_replace( 'https://twitter.com/', 'https://fxtwitter.com/', $_REQUEST['q'] )],
+	);
+}
+
+//$result['result'] = array_merge($result['result'], [ 'curl_result' => file_get_contents($result['result']['text']) ]);
+
 echo json_encode($result);
 
