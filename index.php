8cef43f6a4bc09e23ee5e127f71228fb1203a367
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2024-05-14 19:34:27 +0900

    function push2discord
    https://github.com/n138-kz/http_post/tree/main/php

--- index.php
+++ index.php
@@ -6,6 +6,46 @@ session_start();
 function is_empty($arg) {
 	return ( !isset($arg) || empty($arg) );
 }
+function push2discord($endpoint, $content_author='Webhooks', $content_author_avatar='https://www.google.com/s2/favicons?size=256&domain=https://discord.com/', $content_color=0, $content_body=''){
+	if ( is_empty( $endpoint ) ) { return false; }
+	$content_color = is_numeric($content_color) ? $content_color : 0;
+	$content_color = $content_color > 0 ? $content_color : 0;
+
+	$payload = [];
+	$payload += [
+		'username' => $content_author,
+		'content' => chr(0),
+		'avatar_url' => $content_author_avatar,
+		'embeds' => [],
+	];
+	array_push($payload['embeds'], [
+		'color' => $content_color,
+		'timestamp' => date('c'),
+		'footer' => [
+		'text' => 'Auth-Google'
+		],
+		'fields' => [
+		[
+			'inline' => false,
+			'name' => '',
+			'value' => $content_body
+		]
+		]
+	]);
+	$payload_encoded = json_encode($payload);
+	$curl_req = curl_init($endpoint);
+	curl_setopt($curl_req,CURLOPT_POST, TRUE);
+	curl_setopt($curl_req,CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+	curl_setopt($curl_req,CURLOPT_POSTFIELDS, $payload_encoded);
+	curl_setopt($curl_req,CURLOPT_SSL_VERIFYPEER, TRUE);
+	curl_setopt($curl_req,CURLOPT_SSL_VERIFYHOST, 2);
+	curl_setopt($curl_req,CURLOPT_RETURNTRANSFER, TRUE);
+	curl_setopt($curl_req,CURLOPT_FOLLOWLOCATION, TRUE);
+	$curl_res=curl_exec($curl_req);
+	$curl_res=json_decode($curl_res, TRUE);
+	return $curl_res;
+}
+
 $system = [
 	'hash_aglos' => 'sha1',
 ];
